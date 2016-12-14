4b8a1640d6ebe69dc96720b533378c3599c83406
kylelingat <kylejlingat@gmail.com>
2016-12-14 09:07:16 -1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+function red() {
+  document.getElementById("box").style.backgroundColor = "red";
+}
+
+function black() {
+  document.getElementById("box").style.backgroundColor = "black";
+}
+
+function orange() {
+  document.getElementById("box1").style.backgroundColor = "orange";
+}
+
+function black1() {
+  document.getElementById("box1").style.backgroundColor = "black";
+}
+
+function green() {
+  document.getElementById("box2").style.backgroundColor = "green";
+}
+
+function black2() {
+  document.getElementById("box2").style.backgroundColor = "black";
+}
+
+function blue() {
+  document.getElementById("box3").style.backgroundColor = "blue";
+}
+
+function black3() {
+  document.getElementById("box3").style.backgroundColor = "black";
+}
+
